08c11781122b2fd4abf51e65a1a998d53e5503b9
Axl <axlrose.lopez@gmail.com>
2019-07-07 08:15:43 +0800

    Update detail page

--- src/pages/PledgeDetailPage.js
+++ src/pages/PledgeDetailPage.js
@@ -4,6 +4,7 @@ import { withRouter } from "react-router-dom";
 
 import { withFirebase } from "../components/Firebase";
 import SignOutButton from "../components/SignOutButton/SignOutButton";
+import IconWSeeed from '../assets/images/white-seed.png';
 
 const INITIAL_STATE = {
   pledge: [],
@@ -43,18 +44,138 @@ class PledgeDetailPage extends Component {
   render = () => {
     return (
       <Fragment>
-        <p>{this.state.pledge.id}</p>
-        <p>{this.state.pledge.title}</p>
-        <p>{this.state.pledge.description}</p>
-        <p>{this.state.pledge.duration}</p>
-        <p>{this.state.pledge.created_at}</p>
-        <p>{this.state.pledge.updated_at}</p>
-        <p>{this.state.pledge.category}</p>
-        {this.state.pledge.posts &&
-          this.state.pledge.posts.map(item => <p>item</p>)}
-        <button onClick={() => this.joinPledge(this.state.pledge.id)}>
-          Join Pledge
-        </button>
+        <div className="wrapper pledge-detail">
+          <div className="banner">
+            <div className="info">
+              <ul>
+                <li>Status:</li>
+                <li>1 of 30</li>
+              </ul>
+              <ul>
+                <li>Duration:</li>
+                <li>{this.state.pledge.duration}</li>
+              </ul>
+            </div>
+            <button>Done</button>
+          </div>
+          <div className="top">
+            <ul className="total-pledge">
+              <li>1, 987</li>
+              <li>Total Pledges</li>
+            </ul>
+            <button><img src={IconWSeeed} alt="White Seed" />Check my Seed</button>
+          </div>
+          <div className="details">
+            <h4>Details</h4>
+            <p>{this.state.pledge.description}</p>
+            <h4>Discussion</h4>
+            <div className="discussion">
+              <ul>
+                <li><div className="profile"></div></li>
+                <li>
+                  <h4>Antonio Wilson</h4>
+                  <p>I’ve really had a good time. I was able to push myself to do more and be more.</p>
+                  <ul>
+                    <li><button>Like</button></li>
+                    <li><button>Reply</button></li>
+                  </ul>
+                </li>
+                <li>9:03 am</li>
+              </ul>
+            </div>
+            {/* <p>{this.state.pledge.id}</p> */}
+            {/* <p>{this.state.pledge.title}</p> */}
+            {/* <p>{this.state.pledge.created_at}</p>
+            <p>{this.state.pledge.updated_at}</p> */}
+            {/* <p>{this.state.pledge.category}</p> */}
+            {this.state.pledge.posts &&
+              this.state.pledge.posts.map(item => <p>item</p>)}
+            <button onClick={() => this.joinPledge(this.state.pledge.id)}>
+              Join Pledge
+          </button>
+          </div>
+          <div className="details">
+            <h4>Details</h4>
+            <p>{this.state.pledge.description}</p>
+            <h4>Discussion</h4>
+            <div className="discussion">
+              <ul>
+                <li><div className="profile"></div></li>
+                <li>
+                  <h4>Antonio Wilson</h4>
+                  <p>I’ve really had a good time. I was able to push myself to do more and be more.</p>
+                  <ul>
+                    <li><button>Like</button></li>
+                    <li><button>Reply</button></li>
+                  </ul>
+                </li>
+                <li>9:03 am</li>
+              </ul>
+            </div>
+            {/* <p>{this.state.pledge.id}</p> */}
+            {/* <p>{this.state.pledge.title}</p> */}
+            {/* <p>{this.state.pledge.created_at}</p>
+            <p>{this.state.pledge.updated_at}</p> */}
+            {/* <p>{this.state.pledge.category}</p> */}
+            {this.state.pledge.posts &&
+              this.state.pledge.posts.map(item => <p>item</p>)}
+            <button onClick={() => this.joinPledge(this.state.pledge.id)}>
+              Join Pledge
+          </button>
+          <div className="discussion">
+              <ul>
+                <li><div className="profile"></div></li>
+                <li>
+                  <h4>Antonio Wilson</h4>
+                  <p>I’ve really had a good time. I was able to push myself to do more and be more.</p>
+                  <ul>
+                    <li><button>Like</button></li>
+                    <li><button>Reply</button></li>
+                  </ul>
+                </li>
+                <li>9:03 am</li>
+              </ul>
+            </div>
+            <div className="discussion">
+              <ul>
+                <li><div className="profile"></div></li>
+                <li>
+                  <h4>Antonio Wilson</h4>
+                  <p>I’ve really had a good time. I was able to push myself to do more and be more.</p>
+                  <ul>
+                    <li><button>Like</button></li>
+                    <li><button>Reply</button></li>
+                  </ul>
+                </li>
+                <li>9:03 am</li>
+              </ul>
+            </div>
+            <div className="discussion">
+              <ul>
+                <li><div className="profile"></div></li>
+                <li>
+                  <h4>Antonio Wilson</h4>
+                  <p>I’ve really had a good time. I was able to push myself to do more and be more.</p>
+                  <ul>
+                    <li><button>Like</button></li>
+                    <li><button>Reply</button></li>
+                  </ul>
+                </li>
+                <li>9:03 am</li>
+              </ul>
+            </div>
+            {/* <p>{this.state.pledge.id}</p> */}
+            {/* <p>{this.state.pledge.title}</p> */}
+            {/* <p>{this.state.pledge.created_at}</p>
+            <p>{this.state.pledge.updated_at}</p> */}
+            {/* <p>{this.state.pledge.category}</p> */}
+            {/* {this.state.pledge.posts &&
+              this.state.pledge.posts.map(item => <p>item</p>)}
+            <button onClick={() => this.joinPledge(this.state.pledge.id)}>
+              Join Pledge
+          </button> */}
+          </div>
+        </div>
       </Fragment>
     );
   };
